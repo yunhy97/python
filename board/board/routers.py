46525cf891635520d4b2deef3e8e6b8330671041
Ha young <57880341+yunhy97@users.noreply.github.com>
2021-08-12 18:04:32 +0900

    Delete board/board directory

--- board/board/routers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import routers
-from boardApp import views
-
-'''
-Rest Framework에서는 자동으로 라우팅하는 기능 제공
-사용자의 요청을 확인하고 어디에서 요청이 처리되어야 하는지 알려줌
-'''
-router = routers.DefaultRouter()
-router.register('main', views.post_main)
